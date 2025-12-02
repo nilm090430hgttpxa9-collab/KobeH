fb46debed4e08d6732f2a1ff16c0d6d46cd1e836
nilm090430hgttpxa9 <nilm090430hgttpxa9@cbtis217.edu.mx>
2025-12-01 18:47:16 -0600

    login

---
+++ Gitnew/MainForm.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: MAX
+ * Fecha: 28/11/2025
+ * Hora: 07:33 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gitnew
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void BtnEntrarClick(object sender, EventArgs e)
+		{
+			string contraCorrecta = "123";
+			if(txtContra.Text == contraCorrecta)
+			{
+				frmPrincipal frm = new frmPrincipal();
+				frm.Show();
+				this.Hide();
+			}
+		}
+	}
+}
