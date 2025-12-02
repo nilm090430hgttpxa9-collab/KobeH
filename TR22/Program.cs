76a5c4c5d6c056f290470b2052e8c27ef4e3a4e5
alexisadrian4506-sys <alexisadrian4506@gmail.com>
2025-12-01 18:58:39 -0600

    Promedio

---
+++ TR22/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: manue
+ * Fecha: 21/11/2025
+ * Hora: 02:41 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Windows.Forms;
+
+namespace TR22
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
