504b85f1ffda23dae984b80bb6f141f1c0c36a6f
Cesar847534 <merc090212hgtnmsa3@cbtis217.edu.mx>
2025-12-01 19:28:07 -0600

    generar 10 numeros

---
+++ TRABAJO 3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: cesar
+ * Fecha: 28/11/2025
+ * Hora: 06:22 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Windows.Forms;
+
+namespace TRABAJO_3._
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
